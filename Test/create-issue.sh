5ecdad42c45603adabdadfe28b4858c5ec3b9322
Nick Pellow <npellow@atlassian.com>
2011-03-16 05:42:52 +0000

    Updated test scripts, to match what the jira plugin expects

--- Test/create-issue.sh
+++ Test/create-issue.sh
@@ -1,2 +1,2 @@
 
-curl -F issue=@assets/issue.json -F recording=@assets/jiraconnect-recording.caf -F screenshot=@assets/pony-2.png http://localhost:8080/rest/jconnect/latest/issue
+curl -F issue=@assets/issue.json -F recording=@assets/jiraconnect-recording.caf -F screenshot=@assets/pony-2.png http://localhost:2990/jira/rest/jconnect/latest/issue
